4242eff5ffd96f12abc24bed11577c9a08f12745
softsamiul <softsamiul.com>
2022-10-19 11:05:51 +0530

    header and hero section added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,36 @@ import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+
+      {/* HERO SECTION */}
+      <section id="hero-section" className="flex justify-between items-center bg-yellow-400 border-y border-black py-10">
+
+        <div className="px-10 space-y-5">
+          <h1 className=" text-6xl max-w-xl font-serif "> <span className="underline decoration-black decoration-4">Medium</span> is a place to read, write and connect</h1>
+          <h2>It's easy and free to post your thinking on any topic and connect with millions of hearts.</h2>
+        </div>
+        <div>
+          <img
+            className="hidden md:inline-flex h-32 lg:h-full"
+            src="https://i.ibb.co/gy1QVyJ/medium-logo.png"
+          />
+        </div>
+        
+        <div>
+
+        </div>
+      </section>
+
+      {/* POSTS */}
+
+
+      
     </div>
   );
 };
